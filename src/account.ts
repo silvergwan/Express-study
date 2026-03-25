5d97cb66e04bcb0121768b66dc7fdea90f03d91b
EUNGWANCHOI <silvergwan@gmail.com>
2026-03-25 16:14:27 +0900

    feat: 입금 함수 구현

--- src/account.ts
+++ src/account.ts
@@ -4,3 +4,14 @@ export interface Account {
   readonly accounterNumber: string;
   status: "active" | "frozen" | "closed";
 }
+
+export function deposit(account: Account, amount: number): Account {
+  if (amount <= 0) {
+    throw new Error("입금 금액은 0보다 커야합니다.");
+  }
+  if (account.status !== "active") {
+    throw new Error("활성된 계좌만 입급이 가능합니다.");
+  }
+
+  return { ...account, balance: account.balance + amount };
+}
